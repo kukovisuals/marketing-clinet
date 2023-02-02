a5813c3d62345261437994503434f37bc144b318
Fretz Castano <fretzcastano@Fretzs-MacBook-Pro.local>
2023-02-02 16:23:36 -0500

    cleaning up uplaod

--- src/components/main/upload/upload.tsx
+++ src/components/main/upload/upload.tsx
@@ -12,7 +12,7 @@ function Upload(props: any) {
   const pdpSkuData = useAppSelector((state) => state.profile.mainData)
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-    console.log('handle upload ',event.target)
+    console.log('handle upload ', event.target)
     if (event.target && event.target.files) {
 
       const file = event.target.files[0];
@@ -24,7 +24,6 @@ function Upload(props: any) {
           const parsedData = Papa.parse(csvData);
           // -----------------------------------------------------------
           const comingData: any = parsedData.data
-          const columns = comingData[0];
           let rows = []
 
           for (let i = 1; i < comingData.length; i++) {
@@ -32,53 +31,54 @@ function Upload(props: any) {
             rows.push(comingData[i])
             const rowA = comingData[i][1].split('-')[1]
 
-            let skuArr: TodoType[] = []
-
             dispatch(initObject({ id: i - 1, name: rowA, pdpArr: [] }))
           }
-          
-          setTimeout(() => {
-            for (let i = 1; i < comingData.length; i++) {
-
-              let skuArr: TodoType[] = []
-              for (let column = 2; column < comingData[i].length; column++) {
-  
-                if (comingData[i][column].length > 6) {
-  
-                  for (let k = 0; k < pdpSkuData.length; k++) {
-  
-                    if (k == pdpSkuData.length - 1) {
-                      console.warn("coudln't find product SKU for -> " + comingData[i][column])
-                    }
-                    if (comingData[i][column] == pdpSkuData[k].sku) {
-                      /* 
-                        **********************************************
-                        **         Found sku in pdp payload         **
-                        **********************************************
-                      */
-                      skuArr.push({
-                        id: String(pdpSkuData[k].id),
-                        name: `${pdpSkuData[k].sku}::${pdpSkuData[k].sizeId}`
-                      })
-                      console.log('matching -> ', comingData[i][column], pdpSkuData[k])
-                      k = pdpSkuData.length
-                    }
-                  }
-                }
-              }
-              dispatch(addSelectedPdp({
-                index: i,
-                description: skuArr
-              }))
-            }
-          }, 250)
-          
-          // -----------------------------------------------------------
           props.onData(parsedData.data)
+
+          insertSheetData(comingData)
         }
       };
       reader.readAsText(file);
     }
+  }
+
+  function insertSheetData(comingData: any) {
+
+    setTimeout(() => {
+      for (let i = 1; i < comingData.length; i++) {
+
+        let skuArr: TodoType[] = []
+        for (let column = 2; column < comingData[i].length; column++) {
+
+          if (comingData[i][column].length > 6) {
+
+            for (let k = 0; k < pdpSkuData.length; k++) {
+
+              if (k == pdpSkuData.length - 1) {
+                console.warn("coudln't find product SKU for -> " + comingData[i][column])
+              }
+              if (comingData[i][column] == pdpSkuData[k].sku) {
+                /* 
+                  **********************************************
+                  **         Found sku in pdp payload         **
+                  **********************************************
+                */
+                skuArr.push({
+                  id: String(pdpSkuData[k].id),
+                  name: `${pdpSkuData[k].sku}::${pdpSkuData[k].sizeId}`
+                })
+                console.log('matching -> ', comingData[i][column], pdpSkuData[k])
+                k = pdpSkuData.length
+              }
+            }
+          }
+        }
+        dispatch(addSelectedPdp({
+          index: i,
+          description: skuArr
+        }))
+      }
+    }, 250)
   }
   return (
     <div className="Upload">
